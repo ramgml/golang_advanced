3c8cf9cead141a8442de6c5750a14bc829378a33
Yuriy Tyukalov <5450718+ramgml@users.noreply.github.com>
2025-05-22 16:21:43 +0400

    Added API server

---
+++ 2-random-api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+func main() {
+	router := http.NewServeMux()
+	handler := &RandomHandler{}
+	router.Handle("/dice", handler)
+	fmt.Println("Server is running")
+	http.ListenAndServe(":8081", router)
+}
+
+type RandomHandler struct {}
+
+func (rh *RandomHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	num := fmt.Sprint(rand.Intn(6) + 1)
+	w.Write([]byte(num))
+}
